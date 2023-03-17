e832519fea702dfda270b6be868daf122066760e
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-03-17 14:30:34 +0700

    Tambah file

--- src/main/kotlin/Latihan2.kt
+++ src/main/kotlin/Latihan2.kt
@@ -1,26 +1,23 @@
 fun main () {
-    println("Hello World")
-    println("Mutia")
+//    println("Hello World")
+//    println("Mutia")
+//
+//    print("Pekerjaan : ")
+//    val pekerjaan = readLine()
+//
+//    print("Alamat : ")
+//    val alamat = readLine()
 
-    print("Pekerjaan : ")
-    val pekerjaan = readLine()
+//    print("Pekerjaan kamu adalah $pekerjaan dan alamat kamu $alamat")
 
-    print("Alamat : ")
-    val alamat = readLine()
-
-    print("Pekerjaan kamu adalah $pekerjaan dan alamat kamu $alamat")
-
-}
-
-//Pertemuan2
     var namaLengkap: String = "Mutia Azzahra"
     //Variabel yang kosong harus disebutkan tipe datanya
     var alamat: String
     alamat = "Yogyakarta"
-    //Variabel yang memiliki isi boleh tidak disebutkan tipe
-    datanya
+    //Variabel yang memiliki isi boleh tidak disebutkan tipe datanya
     var umur = 19
     println(namaLengkap)
     println(alamat)
     println(umur)
+
 }
